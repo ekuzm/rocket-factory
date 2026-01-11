ca1e5ad4cb0179f7486f1565ca27ec8fedbf15e3
ekuzm <ecuzmenckov@yandex.by>
2026-01-12 02:19:07 +0300

    Implement payment service

--- payment/cmd/payment-service/main.go
+++ payment/cmd/payment-service/main.go
@@ -1,4 +1,74 @@
 package main
 
+import (
+	"context"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+
+	paymentV1 "github.com/ekuzm/rocket-factory/shared/pkg/proto/payment/v1"
+	"github.com/google/uuid"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+type PaymentService struct {
+	paymentV1.UnimplementedPaymentV1ServiceServer
+}
+
+func NewPaymentService() *PaymentService {
+	return &PaymentService{}
+}
+
+func (p *PaymentService) PayOrder(_ context.Context, req *paymentV1.PayOrderRequest) (*paymentV1.PayOrderResponse, error) {
+	transactionUUID := uuid.New().String()
+
+	log.Printf("The payment was successful, transaction uuid: %s", transactionUUID)
+
+	return &paymentV1.PayOrderResponse{
+		TransactionUuid: transactionUUID,
+	}, nil
+}
+
+const (
+	serverAddress = "127.0.0.1:50052"
+)
+
 func main() {
+	service := NewPaymentService()
+
+	listener, err := net.Listen("tcp", serverAddress)
+	if err != nil {
+		log.Printf("Failed to create listener: %v", err)
+		return
+	}
+	defer func() {
+		if cerr := listener.Close(); cerr != nil {
+			log.Printf("Failed to close listener: %v", cerr)
+		}
+	}()
+
+	server := grpc.NewServer()
+
+	paymentV1.RegisterPaymentV1ServiceServer(server, service)
+	reflection.Register(server)
+
+	go func() {
+		log.Printf("Start gRPC server at %s", serverAddress)
+
+		if err = server.Serve(listener); err != nil {
+			log.Printf("failed to serve grpc server: %v", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	log.Printf("Shutting down the server...")
+	server.GracefulStop()
+
+	log.Printf("Server successfully stopped")
 }
--- payment/go.mod
+++ payment/go.mod
@@ -4,4 +4,16 @@ replace github.com/ekuzm/rocket-factory/shared => ../shared
 
 go 1.25.4
 
-require github.com/google/uuid v1.6.0 // indirect
+require (
+	github.com/ekuzm/rocket-factory/shared v0.0.0-00010101000000-000000000000
+	github.com/google/uuid v1.6.0
+	google.golang.org/grpc v1.78.0
+)
+
+require (
+	golang.org/x/net v0.47.0 // indirect
+	golang.org/x/sys v0.38.0 // indirect
+	golang.org/x/text v0.31.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20251029180050-ab9386a59fda // indirect
+	google.golang.org/protobuf v1.36.10 // indirect
+)
