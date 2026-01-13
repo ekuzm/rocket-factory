40b4271d9baeb080d03ab82ae26d26c2ea530631
ekuzm <ecuzmenckov@yandex.by>
2026-01-13 21:13:45 +0300

    Updated logs info

--- order/internal/middleware/request_logger.go
+++ order/internal/middleware/request_logger.go
@@ -10,10 +10,13 @@ func RequestLogger(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		startTime := time.Now()
 
-		log.Printf("Handler at %v URL with %v Method run", r.URL, r.Method)
+		url := r.URL
+		method := r.Method
+
+		log.Printf("Handler at %v URL with %v Method run", url, method)
 
 		next.ServeHTTP(w, r)
 
-		log.Printf("Handler has worked: %v", time.Since(startTime))
+		log.Printf("Handler at %v URL with %v Method has worked for %v", url, method, time.Since(startTime))
 	})
 }
