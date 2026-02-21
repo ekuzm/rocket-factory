ae9b1c5a3b0ed29a5642046b7f8050a5f8917c40
ekuzm <ecuzmenckov@gmail.com>
2026-02-21 20:52:03 +0300

    Update errors in inventory service interceptor

--- inventory/internal/interceptor/error_mapper.go
+++ inventory/internal/interceptor/error_mapper.go
@@ -8,7 +8,7 @@ import (
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 
-	"github.com/ekuzm/rocket-factory/inventory/internal/model"
+	errs "github.com/ekuzm/rocket-factory/inventory/internal/error"
 )
 
 func MappingErrors() grpc.UnaryServerInterceptor {
@@ -21,9 +21,9 @@ func MappingErrors() grpc.UnaryServerInterceptor {
 		resp, err := handler(ctx, req)
 		if err != nil {
 			switch {
-			case errors.Is(err, model.ErrInvalidFormat):
+			case errors.Is(err, errs.ErrInvalidUUIDFormat):
 				return nil, status.Error(codes.InvalidArgument, err.Error())
-			case errors.Is(err, model.ErrNotFound):
+			case errors.Is(err, errs.ErrPartNotFound):
 				return nil, status.Error(codes.NotFound, err.Error())
 			default:
 				return nil, status.Error(codes.Internal, err.Error())
