143553d3137a33b9bfd0fd2349f1b60b633b13d5
ekuzm <ecuzmenckov@gmail.com>
2026-02-23 20:16:46 +0300

    Update migrations

--- order/migrations/20260213182937_create_orders_table.sql
+++ order/migrations/20260213182937_create_orders_table.sql
@@ -6,13 +6,14 @@ CREATE TABLE IF NOT EXISTS orders(
     total_price DOUBLE PRECISION NOT NULL,
     transaction_uuid UUID NULL,
     status INTEGER NOT NULL CHECK(status BETWEEN 0 AND 2),
-    payment_method INTEGER CHECK(status BETWEEN 0 AND 4),
+    payment_method INTEGER CHECK(payment_method BETWEEN 0 AND 4),
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMP,
     PRIMARY KEY(uuid)
 );
 -- +goose StatementEnd
-DROP TABLE IF EXISTS orders;
+
 -- +goose Down
 -- +goose StatementBegin
+DROP TABLE IF EXISTS orders;
 -- +goose StatementEnd
