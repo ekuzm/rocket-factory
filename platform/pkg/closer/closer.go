9ba0e440cf31538493b983fe1b5a0eced232a78c
Egor Kuzmenkov <egorkuzmenkov@Egors-MacBook-Air.local>
2026-04-01 02:38:46 +0300

    Add logs to closer

--- platform/pkg/closer/closer.go
+++ platform/pkg/closer/closer.go
@@ -7,6 +7,7 @@ import (
 	"sync"
 
 	"github.com/ekuzm/rocket-factory/platform/pkg/logger"
+	"github.com/sirupsen/logrus"
 )
 
 type Func func(context.Context) error
@@ -36,13 +37,17 @@ func CloseAll(ctx context.Context) error {
 		closer.mtx.Unlock()
 
 		if len(funcs) == 0 {
+			logger.Debug("No closer functions registered")
+
 			return
 		}
 
 		var errs []string
 		done := make(chan struct{})
 
-		logger.Debug("Starting closer...")
+		logger.WithFields(logrus.Fields{
+			"closerCount": len(funcs),
+		}).Debug("Starting closer")
 
 		go func() {
 			defer func() {
@@ -67,6 +72,11 @@ func CloseAll(ctx context.Context) error {
 		case <-ctx.Done():
 			out = ctx.Err()
 		case <-done:
+			logger.WithFields(logrus.Fields{
+				"closerCount": len(funcs),
+				"errorCount":  len(errs),
+			}).Debug("Finished closer")
+
 			if len(errs) > 0 {
 				out = fmt.Errorf("closed funcs: %v", strings.Join(errs, " | "))
 			}
