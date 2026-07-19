e5e69665392f59865a16f6d2305adee0cc4162cf
ekuzm <ecuzmenckov@gmail.com>
2026-07-19 17:43:21 +0300

    refactor: simplify grpc healthcheck

--- platform/pkg/grpc/health/health.go
+++ platform/pkg/grpc/health/health.go
@@ -1,28 +1,14 @@
 package health
 
 import (
-	"context"
-
 	"google.golang.org/grpc"
+	grpcHealth "google.golang.org/grpc/health"
 	"google.golang.org/grpc/health/grpc_health_v1"
 )
 
-type Server struct {
-	grpc_health_v1.UnimplementedHealthServer
-}
+func RegisterService(server *grpc.Server, serviceName string) {
+	healthServer := grpcHealth.NewServer()
+	healthServer.SetServingStatus(serviceName, grpc_health_v1.HealthCheckResponse_SERVING)
 
-func (s *Server) Check(ctx context.Context, req *grpc_health_v1.HealthCheckRequest) (*grpc_health_v1.HealthCheckResponse, error) {
-	return &grpc_health_v1.HealthCheckResponse{
-		Status: grpc_health_v1.HealthCheckResponse_SERVING,
-	}, nil
-}
-
-func (s *Server) Watch(req *grpc_health_v1.HealthCheckRequest, stream grpc.ServerStreamingServer[grpc_health_v1.HealthCheckResponse]) error {
-	return stream.Send(&grpc_health_v1.HealthCheckResponse{
-		Status: grpc_health_v1.HealthCheckResponse_SERVING,
-	})
-}
-
-func RegisterService(server *grpc.Server) {
-	grpc_health_v1.RegisterHealthServer(server, &Server{})
+	grpc_health_v1.RegisterHealthServer(server, healthServer)
 }
