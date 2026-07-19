8ed1387e89e5222dc7994a33a5cd15111c569148
ekuzm <ecuzmenckov@gmail.com>
2026-07-19 17:19:19 +0300

    feat: add timeouts into grpc config in payment service

--- payment/internal/config/env/grpc.go
+++ payment/internal/config/env/grpc.go
@@ -3,13 +3,15 @@ package env
 import (
 	"fmt"
 	"net"
+	"time"
 
 	"github.com/caarlos0/env/v11"
 )
 
 type grpcEnvConfig struct {
-	Host string `env:"SERVICE_HOST,required"`
-	Port string `env:"SERVICE_PORT,required"`
+	Host            string        `env:"SERVICE_HOST,required"`
+	Port            string        `env:"SERVICE_PORT,required"`
+	ShutdownTimeout time.Duration `env:"SHUTDOWN_TIMEOUT,required"`
 }
 
 type grpcConfig struct {
@@ -20,10 +22,13 @@ func (gc *grpcConfig) Address() string {
 	return net.JoinHostPort(gc.raw.Host, gc.raw.Port)
 }
 
-func NewGRPCConfig() (*grpcConfig, error) {
-	var raw grpcEnvConfig
+func (gc *grpcConfig) ShutdownTimeout() time.Duration {
+	return gc.raw.ShutdownTimeout
+}
 
-	if err := env.Parse(&raw); err != nil {
+func NewGRPCConfig() (*grpcConfig, error) {
+	raw, err := env.ParseAs[grpcEnvConfig]()
+	if err != nil {
 		return nil, fmt.Errorf("parsing envs: %w", err)
 	}
 
